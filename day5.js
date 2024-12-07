bc7f4bb9d2a9e703acfc62413da915cc8787713e
lakshmi1102-tech <konankilakshmi0211@gmail.com>
2024-12-07 16:10:25 +0530

    object method

---
+++ day5.js
@@ -0,0 +1,60 @@
+
+//  var person = {
+//     firstname:"lucky",
+//     lastname:"konanki",
+//     age:21
+//     }
+//     console.log(person.firstname)
+//     person.city="newyork";
+//     console.log(person)
+
+// var product = {
+//    name:"rice",
+//    price:50,
+//    instock:"20kgs"
+//  }
+//  product.price=60;
+//  console.log(product)
+//  delete product.instock
+//  console.log(product)
+
+// var user = {
+//    username:"lakshmi",
+//    email:"lakshmi@123gmail.com",
+//   address:{
+//    city:vinukonda,
+//    state:Ap,
+//    zip:hello
+//   }
+// }
+// Object.freez(user)
+// console.log(user)
+// Object.steal(user)
+// console.log(object.isfrozen(user.phone=jio))
+
+
+// var car ={
+//   make: "Iron" ,
+//   model: 24 ,
+//   price:"2cr"
+
+// }
+// console.log(object.Seal(car))
+// delete car.make
+// console.log(object.isSealed(car))
+// car.price="5cr";
+// console.log(object.isExtensible(car))
+
+// var a="   lucky";
+// var b="konanki     "
+// var c=a.concat("   "+b).trim().toUpperCase();
+// console.log(c)
+
+// var a="luckykkonanki";
+// var b=a.slice(5,6);
+// var c=b.at(-1)
+// var x=b.indexOf(c)
+// console.log(c.toLowerCase())
+
+// var sentence="hello there, how are you"
+// console.log(sentence.indexOf())
